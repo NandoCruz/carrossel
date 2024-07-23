639918c0e09142aaa06f261c6a845128258a1ffd
Fernando Cruz <nando33_2005@hotmail.com>
2024-07-23 20:18:04 -0300

    atualzar o script e css
    Atualizando o script e o CSS

--- script.js
+++ script.js
@@ -3,16 +3,27 @@ showSlides(index);
 
 function addSlides(n){
     showSlides(index += n);
+<<<<<<< Updated upstream
+=======
+    console.log(showSlides);
+>>>>>>> Stashed changes
 }
 
 function currentSlide(n) {
-    showSlides(index = n)
+    showSlides(index = n);
 }
 
 function showSlides(n){
+<<<<<<< Updated upstream
     let slides = document.getElementsByClassName("slides");
     let dots = document.getElementsByClassName("dot");
         
+=======
+    
+    let slides = document.getElementsByClassName("slides");
+    let dots = document.getElementsByClassName("dot");
+
+>>>>>>> Stashed changes
     if (n > slides.length) {
         index = 1;
     }
@@ -22,7 +33,10 @@ function showSlides(n){
     }
 
     for (let i = 0; i < slides.length; i++) {
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
         slides[i].style.display = 'none';
         dots[i].className = dots[i].className.replace(" active", "");
     }
