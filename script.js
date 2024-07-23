b515b1a16e12a9791603de01d4d76a4c9bc4468f
Fernando Cruz <nando33_2005@hotmail.com>
2024-07-23 15:18:16 -0300

    Correções no Java Script
    Feito correções no Java Script.

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-let index = 1
-let slides = document.getElementsByClassName(".slides");
-let dots = document.getElementsByClassName(".dot");
-
-const btn_proximo = document.querySelector(".button");
-
-const showSlides = 0;
+let index = 1;
+showSlides(index);
 
 function addSlides(n){
     showSlides(index += n);
-    console.log(showSlides);
-    console.log("Adicionando um slide");
 }
-btn_proximo.addEventListener("click", addSlides);
-// showSlides(index);
-/*
-function addSlides(n){
-   // showSlides(index += n);
-    console.log(showSlides);
-}
-/*
+
 function currentSlide(n) {
     showSlides(index = n)
 }
 
 function showSlides(n){
+    let slides = document.getElementsByClassName("slides");
+    let dots = document.getElementsByClassName("dot");
+        
     if (n > slides.length) {
         index = 1;
     }
@@ -32,14 +21,12 @@ function showSlides(n){
         index = slides.length;
     }
 
-    for (let i = 1; i < slides.length; i++) {
-        let slides = document.getElementsByClassName(".slides");
-        let dots = document.getElementsByClassName(".dot");
+    for (let i = 0; i < slides.length; i++) {
 
         slides[i].style.display = 'none';
-        dots[i].className = dots[i].className.replece(".active", " ");
+        dots[i].className = dots[i].className.replace(" active", "");
     }
 
-    slides[index-1].style.display = ".flex";
-    dots[index-1].className += ".active";
-}*/
+    slides[index-1].style.display = "flex";
+    dots[index-1].className += " active";
+}
